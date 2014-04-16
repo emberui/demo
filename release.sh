2fb90eedd5a0593c77859729c7be9769ee67bb10
Jaco Joubert <joubert.jaco@gmail.com>
2014-04-16 16:20:21 -0400

    updated release script for demo

--- release.sh
+++ release.sh
@@ -9,9 +9,6 @@ rm -rf /tmp/dist
 grunt dist         &&
 cp -R dist /tmp/dist  &&
 
-# copy JS files EAK filtered out
-cp -R public/assets/scripts /tmp/dist/assets/scripts &&
-
 # copy 404 page & .gitignore
 cp dist/index.html /tmp/dist/404.html &&
 cp .gitignore /tmp/dist/ &&
@@ -19,14 +16,13 @@ cp .gitignore /tmp/dist/ &&
 # Checkout gh-pages, save what we need, delete the rest and copy over
 git checkout gh-pages    &&
 git pull --rebase origin gh-pages &&
-cp CNAME /tmp/dist/      &&
 cp -R builds /tmp/dist/  &&
 git rm -r ./               &&
 cp -r /tmp/dist/ ./        &&
 
 # Commit and push the changes then switch back to master
 git add . &&
-git commit -am "Update website content" &&
+git commit -am "Update website demo" &&
 git push origin gh-pages  &&
 git checkout master &&
 echo "Website update successful"
